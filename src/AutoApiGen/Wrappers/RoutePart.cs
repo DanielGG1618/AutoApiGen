0cda6a665867ed1f764a60a8d010b0dbd7eee739
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-10 18:01:06 +0400

    feat: route part parsing

--- src/AutoApiGen/Wrappers/RoutePart.cs
+++ src/AutoApiGen/Wrappers/RoutePart.cs
@@ -1,6 +1,4 @@
-﻿using AutoApiGen.Extensions;
-
-namespace AutoApiGen.Wrappers;
+﻿namespace AutoApiGen.Wrappers;
 
 /*[Closed]*/
 public abstract record RoutePart
@@ -13,48 +11,30 @@ public abstract record RoutePart
     public static IImmutableList<RoutePart> Parse(IEnumerable<string> parts) =>
         parts.Select(Parse).ToImmutableArray();
 
-    private static RoutePart Parse(string part)
+    private static RoutePart Parse(string part) => part switch
     {
-        if (!(part.StartsWith("{") && part.EndsWith("}")))
-            return new LiteralRoutePart(part);
+        not ['{', .., '}'] => new LiteralRoutePart(part),
 
-        switch (part.ToList())
-        {
-            case [.., '?', _, _]:
-                return part.Strip(front: 1, back: 2).Split(':').ToList() switch
-                {
-                    [var name] => new OptionalParameterRoutePart(name),
-                    [var name, var type] => new OptionalParameterRoutePart(name, type),
-                    _ => throw new ArgumentException()
-                };
-            case [_, '*', ..]:
-            {
-                var defaultSplit = part
-                    .Strip(front: 1, back: 2)
-                    .Split('=');
+        ['{', .., '}'] when Regexes.RawParameterRoutePartRegex.Match(part) is { Success: true } match =>
+            new RawParameterRoutePart(
+                match.Groups["name"].Value,
+                match.Groups["type"].Value,
+                match.Groups["default"].Value
+            ),
 
-                var typeSplit = defaultSplit[0].Split(':');
+        ['{', .., '?', '}'] when Regexes.OptionalParameterRoutePartRegex.Match(part) is { Success: true } match =>
+            new OptionalParameterRoutePart(
+                match.Groups["name"].Value,
+                match.Groups["type"].Value
+            ),
 
-                return new CatchAllParameterRoutePart(
-                    Name: typeSplit[0],
-                    Type: typeSplit.Length > 1 ? typeSplit[1] : null,
-                    Default: defaultSplit.Length > 1 ? defaultSplit[1] : null
-                );
-            }
-            default:
-            {
-                var defaultSplit = part
-                    .Strip(front: 1, back: 1)
-                    .Split('=');
+        ['{', '*', .., '}'] when Regexes.CatchAllParameterRoutePartRegex.Match(part) is { Success: true } match =>
+            new CatchAllParameterRoutePart(
+                match.Groups["name"].Value,
+                match.Groups["type"].Value,
+                match.Groups["default"].Value
+            ),
 
-                var typeSplit = defaultSplit[0].Split(':');
-
-                return new RawParameterRoutePart(
-                    Name: typeSplit[0],
-                    Type: typeSplit.Length > 1 ? typeSplit[1] : null,
-                    Default: defaultSplit.Length > 1 ? defaultSplit[1] : null
-                );
-            }
-        }
-    }
+        _ => throw new ArgumentException("Invalid syntax", nameof(part))
+    };
 }
