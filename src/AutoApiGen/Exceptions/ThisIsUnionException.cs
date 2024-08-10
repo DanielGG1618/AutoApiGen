7a66b7b4cd747118de5e7c338552a3c5130d2cca
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-10 18:21:44 +0400

    feat: ThisIsUnionException

---
+++ src/AutoApiGen/Exceptions/ThisIsUnionException.cs
@@ -0,0 +1,4 @@
+﻿namespace AutoApiGen.Exceptions;
+
+public class ThisIsUnionException() 
+    : Exception("Waiting for discriminated unions in C# :). Btw if you see this, someone did a terrible thing");
