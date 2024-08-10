9b34baf654bc5e37ea4d034c2ffc0a76d04a3f0d
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-10 18:21:36 +0400

    feat: null instead of empty string when parsing

--- src/AutoApiGen/Wrappers/RoutePart.cs
+++ src/AutoApiGen/Wrappers/RoutePart.cs
@@ -7,7 +7,7 @@ public abstract record RoutePart
     public sealed record RawParameterRoutePart(string Name, string? Type = null, string? Default = null) : RoutePart;
     public sealed record OptionalParameterRoutePart(string Name, string? Type = null) : RoutePart;
     public sealed record CatchAllParameterRoutePart(string Name, string? Type = null, string? Default = null) : RoutePart;
-    
+
     public static IImmutableList<RoutePart> Parse(IEnumerable<string> parts) =>
         parts.Select(Parse).ToImmutableArray();
 
@@ -18,21 +18,21 @@ public abstract record RoutePart
         ['{', .., '}'] when Regexes.RawParameterRoutePartRegex.Match(part) is { Success: true } match =>
             new RawParameterRoutePart(
                 match.Groups["name"].Value,
-                match.Groups["type"].Value,
-                match.Groups["default"].Value
+                match.Groups["type"].Value is { Length: > 0 } type ? type : null,
+                match.Groups["default"].Value is { Length: > 0 } defaultValue ? defaultValue : null
             ),
 
         ['{', .., '?', '}'] when Regexes.OptionalParameterRoutePartRegex.Match(part) is { Success: true } match =>
             new OptionalParameterRoutePart(
                 match.Groups["name"].Value,
-                match.Groups["type"].Value
+                match.Groups["type"].Value is { Length: > 0 } type ? type : null
             ),
 
         ['{', '*', .., '}'] when Regexes.CatchAllParameterRoutePartRegex.Match(part) is { Success: true } match =>
             new CatchAllParameterRoutePart(
                 match.Groups["name"].Value,
-                match.Groups["type"].Value,
-                match.Groups["default"].Value
+                match.Groups["type"].Value is { Length: > 0 } type ? type : null,
+                match.Groups["default"].Value is { Length: > 0 } defaultValue ? defaultValue : null
             ),
 
         _ => throw new ArgumentException("Invalid syntax", nameof(part))
