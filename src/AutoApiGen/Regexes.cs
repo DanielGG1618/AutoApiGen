19b6204723a89cd348860f15b3eb9c2663b31a5f
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-10 17:21:39 +0400

    feat: regexes for route parts parsing

---
+++ src/AutoApiGen/Regexes.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AutoApiGen;
+
+public class Regexes
+{
+    public static readonly Regex RawParameterRoutePartRegex = new(
+        """
+        ^{
+            (?<name>[a-zA-Z_]\w*)
+            (?::(?<type>[a-zA-Z_]\w*))?
+            (?:=(?<default>.+))?
+        }$
+        """,
+        RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace
+    );
+    
+    public static readonly Regex OptionalParameterRoutePartRegex = new(
+        """
+        ^{
+            (?<name>[a-zA-Z_]\w*)
+            (?::(?<type>[a-zA-Z_]\w*))?
+            \?
+        }$
+        """,
+        RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace
+    );
+    
+    public static readonly Regex CatchAllParameterRoutePartRegex = new(
+        """
+        ^{
+            \*
+            (?<name>[a-zA-Z_]\w*)
+            (?::(?<type>[a-zA-Z_]\w*))?
+            (?:=(?<default>.+))?
+        }$
+        """,
+        RegexOptions.Compiled | RegexOptions.IgnorePatternWhitespace
+    );
+}
