65c6a32b96faca4ce100c1ee7a0e7ce176dc2bb1
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-10 18:31:41 +0400

    feat: proper RoutePart formatting

--- src/AutoApiGen/Wrappers/Route.cs
+++ src/AutoApiGen/Wrappers/Route.cs
@@ -1,4 +1,6 @@
-﻿namespace AutoApiGen.Wrappers;
+﻿using AutoApiGen.Exceptions;
+
+namespace AutoApiGen.Wrappers;
 
 public class Route
 {
@@ -7,11 +9,34 @@ public class Route
     public string BaseRoute => _parts[0] is RoutePart.LiteralRoutePart(var value) ? value : "TODO";
 
     public string GetRelationalRoute() =>
-        string.Join(separator: "/", _parts.Skip(1));
+        string.Join(separator: "/", _parts.Skip(1).Select(RoutePartFormatter.Format));
 
     public static Route Wrap(string value) => 
         new(RoutePart.Parse(value.Split('/')));
 
     private Route(IImmutableList<RoutePart> parts) => 
         _parts = parts;
+}
+
+public class RoutePartFormatter
+{
+    public static string Format(RoutePart part) => part switch
+    {
+        RoutePart.LiteralRoutePart(var value) => value,
+        
+        RoutePart.RawParameterRoutePart(var name, var type, var defaultValue) =>
+            $$"""{{{FormatName(name)}}{{FormatType(type)}}{{FormatDefault(defaultValue)}}}""",
+        
+        RoutePart.OptionalParameterRoutePart(var name, var type) => 
+            $$"""{{{FormatName(name)}}{{FormatType(type)}}}""",
+        
+        RoutePart.CatchAllParameterRoutePart(var name, var type, var defaultValue) =>    
+            $$"""{{{FormatName(name)}}{{FormatType(type)}}{{FormatDefault(defaultValue)}}}""",
+        
+        _ => throw new ThisIsUnionException()
+    };
+
+    private static string FormatName(string name) => name;
+    private static string FormatType(string? type) => type is null ? "" : $":{type}";
+    private static string FormatDefault(string? defaultValue) => defaultValue is null ? "" : $"={defaultValue}";
 }
