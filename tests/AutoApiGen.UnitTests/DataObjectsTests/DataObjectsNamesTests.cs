bb9fbe885788ce3c96de315963c29d4e906ae5bb
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-12 02:34:10 +0400

    test: for ITemplateData implementations

---
+++ tests/AutoApiGen.UnitTests/DataObjectsTests/DataObjectsNamesTests.cs
@@ -0,0 +1,22 @@
+﻿using AutoApiGen.DataObjects;
+
+namespace AutoApiGen.UnitTests.DataObjectsTests;
+
+public class DataObjectsNamesTests
+{
+    [Fact]
+    public void TemplateDataImplementationsShouldEndWithData()
+    {
+        // Arrange
+        // Get all classes that implement ITemplateData from AutoApiGen assembly
+        var templateDataImplementations =
+            typeof(ITemplateData).Assembly.GetTypes()
+                .Where(type =>
+                    typeof(ITemplateData).IsAssignableFrom(type)
+                    && type is { IsInterface: false, IsAbstract: false }
+                );
+        
+        // Assert
+        templateDataImplementations.Should().AllSatisfy(type => type.Name.Should().EndWith("Data"));
+    }
+}
