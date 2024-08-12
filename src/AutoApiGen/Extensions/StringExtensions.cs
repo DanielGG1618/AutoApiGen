9f9447dffec512477116f2606287622d39fa11ec
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-12 23:41:17 +0400

    feat: WithLowerFirstLetter string extension

--- src/AutoApiGen/Extensions/StringExtensions.cs
+++ src/AutoApiGen/Extensions/StringExtensions.cs
@@ -8,15 +8,11 @@ internal static class StringExtensions
         1 => str.ToUpperInvariant(),
         _ => char.ToUpperInvariant(str[0]) + str.Substring(1)
     };
-
-    /// <summary>
-    /// Returns string without first <paramref name="front"/> 
-    /// and last <paramref name="back"/> characters
-    /// </summary>
-    /// <param name="str">Source string</param>
-    /// <param name="front">Number of characters to strip from front</param>
-    /// <param name="back">Number of characters to strip from back</param>
-    /// <returns></returns>
-    public static string Strip(this string str, int front, int back) =>
-        str.Remove(str.Length - back).Substring(front);
+    
+    public static string WithLowerFirstLetter(this string str) => str.Length switch
+    {
+        0 => str,
+        1 => str.ToLowerInvariant(),
+        _ => char.ToLowerInvariant(str[0]) + str.Substring(1)
+    };
 }
