a793e2707887a177854a27d4e7d272c5195e7757
Daniel Gevorgyan <57752868+DanielGG1618@users.noreply.github.com>
2024-08-15 22:43:17 +0400

    ci: experiment with release.config.js

--- release.config.js
+++ release.config.js
@@ -1,23 +1,8 @@
-module.exports = 
 {
-    verifyConditions: 
+    "plugins":
     [
         '@semantic-release/changelog',
         '@semantic-release/git',
         '@semantic-release/github',
-    ],
-  
-    publish: 
-    [
-        '@semantic-release/changelog',
-        '@semantic-release/git',
-        {
-            path: '@semantic-release/exec',
-            cmd: `dotnet nuget push ${process.env.ARTIFACTS_DIRECTORY}/*.nupkg --source "github" --skip-duplicate`
-        },
-        {
-            path: '@semantic-release/github',
-            assets: `${process.env.ARTIFACTS_DIRECTORY}/*.nupkg`,
-        },
     ],
 }
