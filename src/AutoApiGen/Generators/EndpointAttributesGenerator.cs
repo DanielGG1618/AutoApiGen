cc4a1659739fd1c88b2696f4c5da5c5603f9609c
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-21 02:08:26 +0400

    refactor: made generated endpoint attributes internal

--- src/AutoApiGen/Generators/EndpointAttributesGenerator.cs
+++ src/AutoApiGen/Generators/EndpointAttributesGenerator.cs
@@ -16,30 +16,30 @@ internal class EndpointAttributesGenerator : IIncrementalGenerator
 
                 #pragma warning disable CS9113 // Parameter is unread.
                 [global::System.AttributeUsage(global::System.AttributeTargets.Class, AllowMultiple = true)]
-                public abstract class EndpointAttribute(
+                internal abstract class EndpointAttribute(
                     [global::System.Diagnostics.CodeAnalysis.StringSyntax("Route")] string route
                 ) : global::System.Attribute;
                 #pragma warning restore CS9113 // Parameter is unread.
                 
-                public sealed class GetEndpointAttribute(
+                internal sealed class GetEndpointAttribute(
                     [global::System.Diagnostics.CodeAnalysis.StringSyntax("Route")] string route = ""
                 ) : global::AutoApiGen.Attributes.EndpointAttribute(route);
-                public sealed class PostEndpointAttribute(
+                internal sealed class PostEndpointAttribute(
                     [global::System.Diagnostics.CodeAnalysis.StringSyntax("Route")] string route = ""
                 ) : global::AutoApiGen.Attributes.EndpointAttribute(route);
-                public sealed class PutEndpointAttribute(
+                internal sealed class PutEndpointAttribute(
                     [global::System.Diagnostics.CodeAnalysis.StringSyntax("Route")] string route = ""
                 ) : global::AutoApiGen.Attributes.EndpointAttribute(route);
-                public sealed class DeleteEndpointAttribute(
+                internal sealed class DeleteEndpointAttribute(
                     [global::System.Diagnostics.CodeAnalysis.StringSyntax("Route")] string route = ""
                 ) : global::AutoApiGen.Attributes.EndpointAttribute(route);
-                public sealed class HeadEndpointAttribute(
+                internal sealed class HeadEndpointAttribute(
                     [global::System.Diagnostics.CodeAnalysis.StringSyntax("Route")] string route = ""
                 ) : global::AutoApiGen.Attributes.EndpointAttribute(route);
-                public sealed class PatchEndpointAttribute(
+                internal sealed class PatchEndpointAttribute(
                     [global::System.Diagnostics.CodeAnalysis.StringSyntax("Route")] string route = ""
                 ) : global::AutoApiGen.Attributes.EndpointAttribute(route);
-                public sealed class OptionsEndpointAttribute(
+                internal sealed class OptionsEndpointAttribute(
                     [global::System.Diagnostics.CodeAnalysis.StringSyntax("Route")] string route = ""
                 ) : global::AutoApiGen.Attributes.EndpointAttribute(route);
                 """
