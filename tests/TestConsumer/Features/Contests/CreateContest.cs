07767d7953835d3255ecfe5e6f0d1cefd3ddfd29
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-08-25 13:17:02 +0300

    docs: added status codes specification to DesiredFunctionality.md

--- tests/TestConsumer/Features/Contests/CreateContest.cs
+++ tests/TestConsumer/Features/Contests/CreateContest.cs
@@ -8,7 +8,13 @@ namespace TestConsumer.Features.Contests;
 public static class CreateContest
 {
     [PostEndpoint("contests/{Id:int=56}")]
-    public record Command(string Name, bool Status = false, int Id = 56, decimal Price = 12.42m, float Currency = 1.23f) : IRequest<Contest>;
+    public record Command(
+        string Name,
+        bool Status = false,
+        int Id = 56,
+        decimal Price = 12.42m,
+        float Currency = 1.23f
+    ) : IRequest<Contest>;
 
     public class Handler : IRequestHandler<Command, Contest>
     {
