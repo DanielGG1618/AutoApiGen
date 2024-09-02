1f407096c17e0f61cfb56b14f549389a14bb7cdc
Daniel Gevorgyan <daniel.gevorgyan25@gmail.com>
2024-09-03 00:45:34 +0300

    refactor: proper name for result type config creator method

--- src/AutoApiGen/Generators/Providers.cs
+++ src/AutoApiGen/Generators/Providers.cs
@@ -29,39 +29,9 @@ internal static class Providers
             && attribute.Name.ToString().Contains("ResultTypeConfiguration"),
 
         transform: static (syntaxContext, _) =>
-            Method((AttributeSyntax)syntaxContext.Node)
+            ResultTypeConfig.TryCreate((AttributeSyntax)syntaxContext.Node)
     );
-
-    private static ResultTypeConfig? Method(AttributeSyntax attribute)
-    {
-        var arguments = attribute.ArgumentList?.Arguments
-            .Where(arg => arg.NameEquals is not null)
-            .ToImmutableDictionary(
-                keySelector: arg =>
-                    arg.NameEquals!.Name.Identifier.Text,
-                elementSelector: arg =>
-                    arg.Expression is LiteralExpressionSyntax literal ? literal.Token.ValueText : null
-            );
-        
-        return arguments is null ? null
-            : new ResultTypeConfig(
-                TypeName: arguments["TypeName"]
-                          ?? throw new ArgumentException("TypeName is missing"),
-                MatchMethodName: arguments["MatchMethodName"]
-                                 ?? "Match",
-                ErrorHandlerMethod(arguments)
-            );
-
-        (string Name, string Implementation)? ErrorHandlerMethod(ImmutableDictionary<string, string?> args) =>
-            args["ErrorHandlerMethodName"] is string name
-            && args["ErrorHandlerMethodImplementation"] is string implementation
-                ? (
-                    name,
-                    implementation
-                ) 
-                : null;
-    }
-
+    
     public static IncrementalValuesProvider<EndpointContractModel> CreateEndpointsProvider(
         this SyntaxValueProvider syntaxValueProvider
     ) => syntaxValueProvider.CreateSyntaxProvider(
--- src/AutoApiGen/Generators/ResultTypeConfig.cs
+++ src/AutoApiGen/Generators/ResultTypeConfig.cs
@@ -1,7 +1,41 @@
-﻿namespace AutoApiGen.Generators;
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace AutoApiGen.Generators;
 
 internal readonly record struct ResultTypeConfig(
     string TypeName,
     string MatchMethodName,
     (string Name, string Implementation)? ErrorHandlerMethod
-);
+)
+{
+    public static ResultTypeConfig? TryCreate(AttributeSyntax attribute)
+    {
+        var arguments = attribute.ArgumentList?.Arguments
+            .Where(arg => arg.NameEquals is not null)
+            .ToImmutableDictionary(
+                keySelector: arg =>
+                    arg.NameEquals!.Name.Identifier.Text,
+                elementSelector: arg =>
+                    arg.Expression is LiteralExpressionSyntax literal ? literal.Token.ValueText : null
+            );
+        
+        return arguments is null ? null
+            : new ResultTypeConfig(
+                TypeName: arguments["TypeName"]
+                ?? throw new ArgumentException("TypeName is missing"),
+                MatchMethodName: arguments["MatchMethodName"]
+                ?? "Match",
+                ErrorHandlerMethod(arguments)
+            );
+
+        (string Name, string Implementation)? ErrorHandlerMethod(ImmutableDictionary<string, string?> args) =>
+            args["ErrorHandlerMethodName"] is string name
+            && args["ErrorHandlerMethodImplementation"] is string implementation
+                ? (
+                    name,
+                    implementation
+                ) 
+                : null;
+    }
+}
